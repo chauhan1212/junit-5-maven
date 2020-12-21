b9bf69c88a3093c59342e11eb362facce0d422d5
Vijay Chauhan <chauhanvijay81@gmail.com>
2020-12-21 11:47:24 +0530

    added code

---
+++ src/main/java/com/vijay/MathUtils.java
@@ -0,0 +1,25 @@
+package com.vijay;
+
+public class MathUtils {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public double computeCircleArea(double radius) {
+		return Math.PI * radius * radius;
+	}
+
+}
---
+++ src/test/java/com/vijay/MathUtilsTest.java
@@ -0,0 +1,53 @@
+package com.vijay;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	private MathUtils mathUtils;
+
+	@BeforeEach
+	void initEach() {
+		mathUtils = new MathUtils();
+	}
+
+	@Nested
+	class AddTest {
+		@Test
+		void testAddingTwoPositives() {
+			assertEquals(2, mathUtils.add(1, 1), "Add method should return the sum of two numbers");
+		}
+
+		@Test
+		void testAddingTwoNegatives() {
+			assertEquals(-2, mathUtils.add(-1, -1), "Add method should return the sum of two numbers");
+		}
+
+		@Test
+		void testAddingAPositiveAndANegative() {
+			assertEquals(0, mathUtils.add(-1, 1), "Add method should return the sum of two numbers");
+		}
+	}
+
+	@Test
+	void testMultiply() {
+		assertAll(() -> assertEquals(0, mathUtils.multiply(1, 0)), () -> assertEquals(1, mathUtils.multiply(1, 1)),
+				() -> assertEquals(6, mathUtils.multiply(2, 3)));
+	}
+
+	@Test
+	void computeCircleArea() {
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+	}
+
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
+				"Divide should throw ArithmeticException when denominator is zero");
+	}
+
+}
