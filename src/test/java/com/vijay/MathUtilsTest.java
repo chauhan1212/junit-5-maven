33c7b2894251be4d1402b29946d7e752e61734ae
Vijay Chauhan <chauhanvijay81@gmail.com>
2020-12-22 17:55:54 +0530

    Complete All Course

--- src/test/java/com/vijay/MathUtilsTest.java
+++ src/test/java/com/vijay/MathUtilsTest.java
@@ -1,53 +1,134 @@
 package com.vijay;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestReporter;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
+import org.junit.jupiter.api.condition.OS;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("When running MathUtils")
 class MathUtilsTest {
 
 	private MathUtils mathUtils;
+	TestInfo testInfo;
+	TestReporter testReporter;
+	
+	@BeforeAll
+	//@BeforeAll method must be static unless the test class is annotated with @TestInstance(Lifecycle.PER_CLASS).
+	static void beforeAllInit() {
+		System.out.println("This need to run before All");
+	}
 
 	@BeforeEach
-	void initEach() {
+	void initEach(TestInfo testInfo, TestReporter testReporter) {
 		mathUtils = new MathUtils();
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
+		testReporter.publishEntry("Running "+ testInfo.getDisplayName() + " with Tags :" + testInfo.getTags());
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("Cleaning up...");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("This need to run after All");
 	}
 
 	@Nested
+	@DisplayName("add method")
+	@Tag("Math")
 	class AddTest {
 		@Test
+		@DisplayName("when adding two positive numbers correctly")
 		void testAddingTwoPositives() {
 			assertEquals(2, mathUtils.add(1, 1), "Add method should return the sum of two numbers");
 		}
 
 		@Test
+		@DisplayName("when adding two negetive numbers correctly")
 		void testAddingTwoNegatives() {
 			assertEquals(-2, mathUtils.add(-1, -1), "Add method should return the sum of two numbers");
-		}
-
-		@Test
-		void testAddingAPositiveAndANegative() {
-			assertEquals(0, mathUtils.add(-1, 1), "Add method should return the sum of two numbers");
 		}
 	}
 
 	@Test
+	@Tag("Math")
+	@DisplayName("multiply method")
 	void testMultiply() {
-		assertAll(() -> assertEquals(0, mathUtils.multiply(1, 0)), () -> assertEquals(1, mathUtils.multiply(1, 1)),
-				() -> assertEquals(6, mathUtils.multiply(2, 3)));
+		//System.out.println("Running "+ testInfo.getDisplayName() + " with Tags :" + testInfo.getTags());
+		assertAll(() -> assertEquals(0, mathUtils.multiply(1, 0)), 
+				  () -> assertEquals(1, mathUtils.multiply(1, 1)),
+				  () -> assertEquals(6, mathUtils.multiply(2, 3)));
 	}
 
 	@Test
 	void computeCircleArea() {
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), () -> "Should return right circle area");
 	}
 
 	@Test
+	@DisplayName("Testing divide method")
 	void testDivide() {
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
 				"Divide should throw ArithmeticException when denominator is zero");
 	}
+	
+	@Test
+	@EnabledOnJre(JRE.JAVA_8)
+	@EnabledOnOs(OS.WINDOWS)
+	@DisplayName("Conditional Execution Test Case")
+	//@EnabledIf
+	//@EnabledIfEnvironmentVariable
+	//@EnabledIfSystemProperty
+	void method1() {
+		System.out.println("Condition...");
+	}
+	
+	@Test
+	void testAssumption() {
+		boolean isServerUp = false;
+		assumeTrue(isServerUp);
+		System.out.println("This is assumption test");
+	}
+	
+	@RepeatedTest(3)
+	void testRepeatedAddMethod(RepetitionInfo repetitionInfo) {
+		System.out.println("Total Repetition : "+ repetitionInfo.getTotalRepetitions());
+		System.out.println("Current Repetition : "+ repetitionInfo.getCurrentRepetition());
+		assertEquals(2, mathUtils.add(1, 1));
+	}
+	
+	@Test
+	@Disabled
+	void testDisabled() {
+		fail("This method should not run");
+	}
+	
+	
 
 }
